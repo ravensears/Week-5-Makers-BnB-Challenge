6bf7a3e3ec6355aff232d946344abfec060dc5ef
Naima <naimaosman203@gmail.com>
2022-01-18 19:48:28 +0300

    added a feature test and unit test to, views, connected to database

--- app.rb
+++ app.rb
@@ -1,14 +1,27 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './database_connection_setup'
+require './lib/user'
 
 class Cherbnb < Sinatra::Base
+  enable :sessions
+  
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello World'
+    @user = session[:user]
+    erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    session[:user] = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password], )
+    redirect '/'
   end
 
   run! if app_file == $0
